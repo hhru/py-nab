7bae74249c2df764365e151f307cccc859805b59
Alexei Pertsev <oeermanz@gmail.com>
2024-05-29 23:04:09 +0300

    HH-217655 add docker network fixture

--- almatest/almatest/containers.py
+++ almatest/almatest/containers.py
@@ -1,7 +1,10 @@
 import asyncio
+import uuid
 from concurrent.futures import ThreadPoolExecutor
 from typing import TypeVar
 
+import pytest
+from docker.models.networks import Network
 from testcontainers.core.config import testcontainers_config
 from testcontainers.core.container import DockerContainer
 
@@ -18,3 +21,12 @@ async def start_container(container: C) -> C:
 
     with ThreadPoolExecutor() as pool:
         return await asyncio.get_running_loop().run_in_executor(pool, _start_container)
+
+
+@pytest.fixture(scope="session")
+def container_network() -> Network:
+    client = docker.from_env()
+    network_name = f"test_{uuid.uuid4().hex}"
+    network = client.networks.create(network_name)
+    yield network
+    network.remove()
